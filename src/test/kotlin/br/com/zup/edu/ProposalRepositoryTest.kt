ba7ec4104727cf6095b8a9116da7ca661669b5ec
Rafael Ponte <rponte@gmail.com>
2021-06-09 13:28:44 -0300

    improves the code;

--- src/test/kotlin/br/com/zup/edu/ProposalRepositoryTest.kt
+++ src/test/kotlin/br/com/zup/edu/ProposalRepositoryTest.kt
@@ -1,23 +1,20 @@
 package br.com.zup.edu
 
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import java.util.*
 
 @MicronautTest
-class ProposalRepositoryTest(val repository: ProposalRepository) {
-
-    val CUSTOMER_ID = UUID.randomUUID()
+class ProposalRepositoryTest(private val repository: ProposalRepository) {
 
     @Test
     fun `should find by id`() {
         // scenario
         val proposal = repository.save(Proposal(
             name = "p1",
-            customerId = CUSTOMER_ID
+            customerId = UUID.randomUUID()
         ))
 
         // action
@@ -27,7 +24,7 @@ class ProposalRepositoryTest(val repository: ProposalRepository) {
         with(found) {
             assertTrue(isPresent)
             assertEquals(proposal.id, get().id)
-            assertEquals(CUSTOMER_ID, get().customerId)
+            assertEquals(proposal.customerId, get().customerId)
         }
     }
 
@@ -36,7 +33,7 @@ class ProposalRepositoryTest(val repository: ProposalRepository) {
         // scenario
         val proposal = repository.save(Proposal(
             name = "p1",
-            customerId = CUSTOMER_ID
+            customerId = UUID.randomUUID()
         ))
 
         // action
@@ -49,7 +46,7 @@ class ProposalRepositoryTest(val repository: ProposalRepository) {
         with(found) {
             assertTrue(isPresent)
             assertEquals(proposal.id, get().id)
-            assertEquals(CUSTOMER_ID, get().customerId)
+            assertEquals(proposal.customerId, get().customerId)
         }
     }
 
